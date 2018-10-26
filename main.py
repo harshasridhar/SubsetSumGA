6d49ac4822f6477354f1a737087f03f5d1f909a0
Harsha M S <sridharsha598@gmail.com>
2018-10-26 14:33:41 +0530

    Population Generation code added

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import numpy as np 
-n = input('Enter the population size: ')
-sum = input('Enter the sum to be obtained: ')
+import math
+def fitness(array,data,sum):
+    c=0
+    for i in range(len(data)):
+        if(array[i] == 1):
+            c += data[i]
+    print('sum :'+ str(c)+"\tDifference :" +str(c-sum))
+    if c == sum:
+        return 100
+    else :
+        return (1/abs(c-sum))*100
+# limit = input('Enter the max limit: ')
+# n = input('Enter the population size: ')
+# sum = input('Enter the sum to be obtained: ')
+# population = []
+# for i in range(n):
+n = 5
+sum = 35
+data = [1,2,3,4,5,6,7,8,9,10]
 population = []
 for i in range(n):
-    
+    population.append(np.random.randint(2,size = len(data)))
+print('Required Sum : '+str(sum))
+for i in range(n):
+    print(population[i])
+    print("Fitness Value: ", str(fitness(population[i],data,sum)),"\n")
