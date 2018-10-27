1f517a85705f8ef3ff820236b78406964ffa273d
Harsha M S <sridharsha598@gmail.com>
2018-10-27 12:35:57 +0530

    Crossover operation added

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import numpy as np 
 import math
 from operator import itemgetter
+import random
+solutions = []
+def crossover(parent1 , parent2):
+    point =5
+    child1 = np.concatenate((parent1[:point],parent2[point:]),axis=None)
+    child2 = np.concatenate((parent2[:point],parent1[point:]),axis=None)
+    return child1,child2
 def fitness(array,data,sum):
     c=0
     for i in range(len(data)):
@@ -16,13 +23,17 @@ def fitness(array,data,sum):
 # sum = input('Enter the sum to be obtained: ')
 # population = []
 # for i in range(n):
-n = 5
+n = 20
 sum = 35
 data = [1,2,3,4,5,6,7,8,9,10]
+mutation_rate = 0.2
+crossover_rate = 0.4
 population = []
 for i in range(n):
     population.append({"chromosome" :np.random.randint(2,size = len(data)), "fitness" : 0})
     population[i]["fitness"] = fitness(population[i]['chromosome'],data,sum)
+    if(int(population[i]['fitness']) == 100):
+        solutions.append(population[i]['chromosome'])
 print('Required Sum : '+str(sum))
 #sort the population by the fitness values
 population = sorted(population, key = itemgetter('fitness'))
@@ -44,3 +55,13 @@ for i in range(len(population)):
         new_population.append(population[i])
 # new population after ranking process
 print(new_population)
+
+num_strings_for_crossover = round(len(new_population)*crossover_rate)
+num_strings_for_crossover = num_strings_for_crossover+1 if num_strings_for_crossover%2!=0 else num_strings_for_crossover
+strings_selected = random.sample(range(0,len(new_population)),num_strings_for_crossover)
+print("Number of strings selected : " +str(num_strings_for_crossover))
+print(strings_selected)
+print( solutions)
+#crossover operation
+for i in range(int(len(strings_selected)/2)):
+    population[i]["chromosome"],population[len(strings_selected)-i]["chromosome"] = crossover(population[i]["chromosome"],population[len(strings_selected)-i]["chromosome"])
