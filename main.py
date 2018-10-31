df96b4d763044fa0b7b00fa4d1b1677b567b1191
Harsha M S <sridharsha598@gmail.com>
2018-10-31 19:05:36 +0530

    Code Added for Mutation

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
-import numpy as np 
+import numpy as np
 import math
 from operator import itemgetter
 import random
 solutions = []
+def appendSolution(solution):
+    if len(solutions) == 0:
+        solutions.append(solution)
+    else:
+        diff = 0
+        for sol in solutions:
+                if(np.array_equiv(np.array(sol),np.array(solution)) == False):
+                        diff += 1
+        if(diff == len(solutions)):
+                solutions.append(solution)
+        
 def crossover(parent1 , parent2):
     point =5
     child1 = np.concatenate((parent1[:point],parent2[point:]),axis=None)
     child2 = np.concatenate((parent2[:point],parent1[point:]),axis=None)
-    return child1,child2
+    return np.array(child1),np.array(child2)
+def mutation(parent, mask):
+    child=[]
+    for i in range(len(parent)):
+        child.append( parent[i] if mask[i]==1 else int(not parent[i]))
+    return child
 def fitness(array,data,sum):
     c=0
     for i in range(len(data)):
         if(array[i] == 1):
             c += data[i]
-    # print('sum :'+ str(c)+"\tDifference :" +str(c-sum))
     if c == sum:
-        return 100
+        return 100.0
     else :
         return (1/abs(c-sum))*100
 # limit = input('Enter the max limit: ')
@@ -32,8 +47,8 @@ population = []
 for i in range(n):
     population.append({"chromosome" :np.random.randint(2,size = len(data)), "fitness" : 0})
     population[i]["fitness"] = fitness(population[i]['chromosome'],data,sum)
-    if(int(population[i]['fitness']) == 100):
-        solutions.append(population[i]['chromosome'])
+#     if(int(population[i]['fitness']) == 100):
+        # appendSolution(population[i]['chromosome'])
 print('Required Sum : '+str(sum))
 #sort the population by the fitness values
 population = sorted(population, key = itemgetter('fitness'))
@@ -62,6 +77,32 @@ strings_selected = random.sample(range(0,len(new_population)),num_strings_for_cr
 print("Number of strings selected : " +str(num_strings_for_crossover))
 print(strings_selected)
 print( solutions)
+
+print('\n\n')
+print(new_population)
 #crossover operation
 for i in range(int(len(strings_selected)/2)):
-    population[i]["chromosome"],population[len(strings_selected)-i]["chromosome"] = crossover(population[i]["chromosome"],population[len(strings_selected)-i]["chromosome"])
+    new_population[i]["chromosome"],new_population[len(strings_selected)-i]["chromosome"] = crossover(new_population[i]["chromosome"],new_population[len(strings_selected)-i]["chromosome"])
+print('\n\n')
+for i in range(len(new_population)):
+        new_population[i]['fitness']=fitness(new_population[i]['chromosome'],data,sum)
+# print(new_population)
+
+# mutation
+number_of_mutants = round(mutation_rate * len(new_population))
+print(number_of_mutants)
+chromosomes_selected = random.sample(range(0,len(new_population)),number_of_mutants)
+print(chromosomes_selected)
+mask=np.random.randint(2,size = len(data))
+for i in chromosomes_selected:
+        new_population[i]['chromosome']=np.array(mutation(new_population[i]['chromosome'],mask))
+        new_population[i]['fitness'] = fitness(new_population[i]['chromosome'],data,sum)
+new_population = sorted(new_population, key = itemgetter('fitness'))
+for i in range(len(new_population)):
+        if(int(new_population[i]['fitness']) == 100):
+                appendSolution(new_population[i]['chromosome'])
+print('\n\nNew Population after one iteration\n')   
+for individual in new_population:
+        print(individual)
+print('\n\n')
+print(solutions)
